5f9a803d7169a96fe3f8ac73bd90549903303d34
isml26 <iguven1999@hotmail.com>
2024-04-25 22:36:03 +0300

    docker-compose & job update

--- src/main/java/FlinkCommerce/DataStreamJob.java
+++ src/main/java/FlinkCommerce/DataStreamJob.java
@@ -21,10 +21,14 @@ package FlinkCommerce;
 import Deserializer.JSONValueDeserializationSchema;
 import Dto.Transaction;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class DataStreamJob {
@@ -47,10 +51,15 @@ public class DataStreamJob {
             .build();
 
     DataStream<Transaction> transactionDataStream =
-            env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Stream");
+            env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
 
         transactionDataStream.print();
-
+/*
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(
+                3, // number of restart attempts
+                Time.of(10, TimeUnit.SECONDS) // delay
+        ));
+  */
         env.execute("Flink Java API");
     }
 }
